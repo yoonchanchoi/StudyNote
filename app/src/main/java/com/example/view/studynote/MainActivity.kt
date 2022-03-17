584d88045f5e9de5464b8ba4d37fb0214056eca7
Choi YoonChan <choyc9239@gmail.com>
2022-03-17 19:04:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/view/studynote/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.view.studynote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+
+    // 기본 작업 모음에 탐색 지원을 추가하려면 아래와 같이 기본 활동의 onCreate() 메서드에서 setupActionBarWithNavController()를 호출합니다. AppBarConfiguration은 onSupportNavigateUp()을 재정의할 때도 사용하므로 onCreate() 외부에 선언해야 합니다.
+    // 즉 add 프래그먼트로 갔을 때 상단 왼쪽에 툴바 안에 있는 뒤로 가기 버튼을 누르면 navHostFragment로 돌아옵니다.
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
