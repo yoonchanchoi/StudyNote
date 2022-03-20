5c4eaffa1de405c8a2bf97f4986eb207784a4df4
Choi YoonChan <choyc9239@gmail.com>
2022-03-21 03:47:22 +0900

    test

--- app/src/main/java/com/example/view/studynote/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/view/studynote/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.example.view.studynote.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.view.studynote.R
 
 
@@ -15,9 +13,16 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //메뉴 세팅
+        setHasOptionsMenu(true)
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu,menu)
+    }
 }
